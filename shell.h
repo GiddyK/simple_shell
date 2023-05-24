55f266dca84b29adeef42c1b9bc85ba4fe895893
Koech Gideon kipkorir <116116298+GiddyK@users.noreply.github.com>
2023-05-24 14:39:44 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -116,6 +116,8 @@ int hsh(info_t *, char **);
 int find_builtin(info_t *);
 void find_cmd(info_t *);
 void fork_cmd(info_t *);
+void _puts(char *);
+int _putchar(char);
 
 /* toem_Parser.c */
 int is_cmd(info_t *, char *);
@@ -180,12 +182,16 @@ int _myexit(info_t *);
 int _mycd(info_t *);
 int _myhelp(info_t *);
 void _eputs(char *);
+int _putchar(char);
+void _puts(char *);
 
 /* toem_Builtins1.c */
 int _myhistory(info_t *);
 int _myalias(info_t *);
 void _eputs(char *);
 int _eputchar(char);
+void _puts(char *);
+int _putchar(char);
 
 /*toem_Getline.c */
 ssize_t get_input(info_t *);
@@ -224,6 +230,7 @@ list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
+char *_strdup(const char *);
 void _puts(char *);
 int _putchar(char);
 
@@ -232,9 +239,7 @@ size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
 size_t print_list(const list_t *);
 list_t *node_starts_with(list_t *, char *, char);
-ssize_t get_node_index(list_t *, list_t *);
-void _puts(char *);
-int _putchar(char);
+size_t get_node_index(list_t *, list_t *);
 
 /* toem_Vars.c */
 int is_chain(info_t *, char *, size_t *);
@@ -242,6 +247,6 @@ void check_chain(info_t *, char *, size_t *, size_t, size_t);
 int replace_alias(info_t *);
 int replace_vars(info_t *);
 int replace_string(char **, char *);
+char *_strdup(const char *);
 
 #endif
-
